35d42f7cc5c557309150c475d305ea7d0e08e4f3
Mohamed Seffine <you@example.com>
2025-05-01 11:47:25 +0100

    structure

--- backend/handlers/upgrade_protocol.go
+++ backend/handlers/upgrade_protocol.go
@@ -1,7 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+var websocketUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 func HandleWS(w http.ResponseWriter, r *http.Request){
-	
+	conn, err := websocketUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		json.NewEncoder(w).Encode(map[string]any{
+			"error":"can't upgrage the protocol",
+		})
+	}
 }
