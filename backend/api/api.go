79c2092455d1211b563be7ec5d7bb2d51ca937a2
Mohamed Seffine <you@example.com>
2025-05-03 12:30:02 +0100

    structure

--- backend/api/api.go
+++ backend/api/api.go
@@ -6,7 +6,8 @@ import (
 )
 
 func Multiplexer() {
-	http.HandleFunc("/ws", handlers.HandleWS)
+	http.HandleFunc("/chat", handlers.HandleWS)
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/frontend/", handlers.HandleStatic)
+	http.HandleFunc("/signup", handlers.HandleSignUp)
 }
