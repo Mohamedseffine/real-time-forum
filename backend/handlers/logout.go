58a5ff39e6029365068c65ef4456837a5fcf56b3
Mohamed Seffine <you@example.com>
2025-05-07 12:30:57 +0100

    cookies

--- backend/handlers/logout.go
+++ backend/handlers/logout.go
@@ -3,22 +3,17 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
-
 	"rt_forum/backend/models"
 )
 
 type logout struct {
 	Id    int    `json:"id"`
-	Username string `json:"username"`
 	Token string `json:"token"`
 }
 
 func LogoutHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if r.Method != http.MethodDelete {
-		fmt.Println(http.MethodDelete)
-		fmt.Println(r.Method)
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(w).Encode(map[string]string{
@@ -29,11 +24,10 @@ func LogoutHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	var data logout
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
-		fmt.Println(err)
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(map[string]string{
-			"error": "method not allowed",
+			"error": "server error",
 		})
 		return
 	}
@@ -46,4 +40,10 @@ func LogoutHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		})
 		return
 	}
+	http.SetCookie(w, &http.Cookie{
+		Name: "token",
+		Value: "",
+		MaxAge: -1,
+
+	})
 }
