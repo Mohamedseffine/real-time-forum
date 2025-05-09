e8fe9056aef7999c569204baba71c033ed529be8
Mohamed Seffine <you@example.com>
2025-05-09 12:54:41 +0100

    get comments

--- backend/handlers/get_comments.go
+++ backend/handlers/get_comments.go
@@ -2,10 +2,35 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
+	"rt_forum/backend/models"
 )
-
-
+type data struct{
+	PostId int `json:"postid"`
+}
+ 
 func RetrieveComments(w http.ResponseWriter, r *http.Request, db *sql.DB)  {
-	
+	if r.Method!=http.MethodGet {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error":"invalid method",
+		})
+		return
+	}
+	var data data
+	json.NewDecoder(r.Body).Decode(&data)
+	comments, err := models.GetComments(db, data.PostId)
+	if err!=nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error":"invalid method",
+		})
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(comments)
 }
--- backend/models/comments.go
+++ backend/models/comments.go
@@ -4,21 +4,41 @@ import (
 	"database/sql"
 	"html"
 	"time"
-)
 
+	"rt_forum/backend/objects"
+)
 
 func InsertComments(db *sql.DB, userid int, postid int, content string) (int, error) {
 	stm, err := db.Prepare(`INSERT INTO comments (creator_id, post_id, creation_date, content) VALUES (?,?,?,?)`)
 	if err != nil {
 		return -1, err
-	}	
+	}
 	res, err := stm.Exec(userid, postid, time.Now(), html.EscapeString(content))
 	if err != nil {
 		return -1, err
 	}
-	id, err:=res.LastInsertId()
+	id, err := res.LastInsertId()
 	if err != nil {
 		return -1, err
 	}
 	return int(id), nil
+}
+
+func GetComments(db *sql.DB, postid int) ([]objects.Comment, error) {
+	rows, err := db.Query(`SELECT * FROM comments WHERE post_id = ?`, postid)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var comments []objects.Comment
+	for rows.Next() {
+		var comment objects.Comment
+		err := rows.Scan(&comment.CommentId, &comment.UserId, &comment.PostId, &comment.Time, &comment.Content)
+		if err != nil {
+			return nil, err
+		}
+		comments = append(comments, comment)
+	}
+
+	return comments, nil
 }
